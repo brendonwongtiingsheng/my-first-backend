b9a80939b4091715c70d110fa5d798e57f4d53b0
Brendon1214 <brooklynwong123@gmail.com>
2025-11-18 17:27:35 +0800

    changes

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require ("express");
 const app = express();
-const PORT = 3000;
 
 app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
 
 const helloRoutes = require("./routes/hello.route");
 const userRoutes = require("./routes/users.route");
@@ -15,5 +16,5 @@ app.use("/auth", authRoutes);
 app.use('/stocks', stocksRoutes);
 
 app.listen(PORT, () => {
-    console.log('Server running at http://localhost:${PORT}');
-})
+  console.log('Server running on port', PORT);
+});
